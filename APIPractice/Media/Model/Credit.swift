3d3c102e88a9df0c803891cb418756a72ef52151
Jin0331 <sempre813@naver.com>
2024-01-18 00:35:17 +0900

    [Add] 폴더 구조 변경 및 empty 파일 생성

---
+++ APIPractice/Media/Model/Credit.swift
@@ -0,0 +1,8 @@
+//
+//  Credit.swift
+//  APIPractice
+//
+//  Created by JinwooLee on 1/18/24.
+//
+
+import Foundation
---
+++ APIPractice/Media/Model/Trend.swift
@@ -0,0 +1,8 @@
+//
+//  Trend.swift
+//  APIPractice
+//
+//  Created by JinwooLee on 1/18/24.
+//
+
+import Foundation
